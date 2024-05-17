17630ea8b147cfa31b3663d560e06fc8e6b763d8
Buruichi <168354904+Buruichi@users.noreply.github.com>
2024-05-17 21:14:23 +0300

    first

---
+++ app/src/main/java/com/example/contacts/MainActivity.kt
@@ -0,0 +1,136 @@
+package com.example.contacts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardDefaults.cardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.contacts.ui.theme.ContactsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column () {
+                ListItem(name = "Имя", prof = "Профессия")
+            }
+        }
+    }
+}
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = cardElevation(5.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Box (
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(74.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(id = R.drawable.doctor),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop)
+                Column (
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = cardElevation(5.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Box(
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(74.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(id = R.drawable.coocker),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = cardElevation(5.dp),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Box (
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(74.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(id = R.drawable.model),
+                    contentDescription = "Image",
+                    contentScale = ContentScale.Crop)
+                Column (
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+}
